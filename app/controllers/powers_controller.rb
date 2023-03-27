dd1072658f35937e0c9d87492a3771a5c77d90c3
jenna-lab <jennifer.sammy@student.moringaschool.com>
2023-03-27 09:55:06 +0300

    added update action for powers

--- app/controllers/powers_controller.rb
+++ app/controllers/powers_controller.rb
@@ -1,4 +1,5 @@
 class PowersController < ApplicationController
+  skip_before_action :verify_authenticity_token, only: [:update]
       # GET /powers
       def index
         render json:Power.all
@@ -13,4 +14,26 @@ def show
     render json: {errors: "Power not found"}, status: :not_found
   end
 end
+
+   #PATCH /powers/:id
+   def update
+    power = find_power
+    if power
+      power.update!(power_params)
+      render json: power
+    else
+      render json: {error: "Power not found"}, status: :not_found
+  end
+end
+
+private
+
+def find_power
+  Power.find_by(id: params[:id])
+end
+
+def power_params
+  params.permit(:name, :description)
+end
+
 end
