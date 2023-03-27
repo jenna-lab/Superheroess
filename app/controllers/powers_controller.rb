692c9b12795e326ae6a3b2e20022d761acd40a58
jenna-lab <jennifer.sammy@student.moringaschool.com>
2023-03-27 09:53:16 +0300

    added show action for powers

--- app/controllers/powers_controller.rb
+++ app/controllers/powers_controller.rb
@@ -3,4 +3,14 @@ class PowersController < ApplicationController
       def index
         render json:Power.all
     end
+
+    #GET /powers/:id
+def show
+  power = Power.find_by(id: params[:id])
+  if power
+  render json: power
+  else
+    render json: {errors: "Power not found"}, status: :not_found
+  end
+end
 end
