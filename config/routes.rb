fab34929524790e1c22f5d4404ab77abcc3d20b9
jenna-lab <jennifer.sammy@student.moringaschool.com>
2023-03-27 09:20:27 +0300

    added heropowers routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   resources :heroes, only: [:index, :show]
+  resources :hero_powers, only: [:create, :index]
 
 end
