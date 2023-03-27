880d967c918046f6a054c5952cee75bcd948ecd4
jenna-lab <jennifer.sammy@student.moringaschool.com>
2023-03-27 09:05:04 +0300

    added faker gem and seeds data

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,31 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
+puts "creating heroes..."
+10.times do
+    Hero.create!(
+      name: Faker::Superhero.name
+    )
+  end
+
+puts "creating powers..."
+10.times do
+    Power.create!(
+      name: Faker::Superhero.power,
+      description: Faker::Lorem.paragraph(sentence_count: 5)
+    )
+  end
+
+puts "creating hero_powers..."
+Hero.all.each do |hero|
+    Power.all.sample(3).each do |power|
+      HeroPower.create!(
+        hero: hero,
+        power: power,
+        strength: Faker::Base.sample(['Strong', 'Weak', 'Average'])
+      )
+    end
+  end
+  
+
+puts "done seeding..."
