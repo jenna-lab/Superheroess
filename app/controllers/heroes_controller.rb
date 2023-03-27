4694bffe1626d30e9123dd1e79c72321d2d95e06
jenna-lab <jennifer.sammy@student.moringaschool.com>
2023-03-27 18:34:01 +0300

    heroes includes powers

--- app/controllers/heroes_controller.rb
+++ app/controllers/heroes_controller.rb
@@ -1,14 +1,14 @@
 class HeroesController < ApplicationController
        #GET /heroes
        def index
-        render json:Hero.all
+        render json:Hero.all.to_json(include: :powers)
     end
 
       #GET /heroes/:id
       def show
         hero = Hero.find_by(id: params[:id])
         if hero
-        render json: hero
+        render json: hero.to_json(include: :powers)
         else
           render json: {errors: "Hero not found"}, status: :not_found
         end
