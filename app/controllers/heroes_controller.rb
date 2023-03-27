0ffb6ca0ce431bf97111537f240bd5a04a0807a8
jenna-lab <jennifer.sammy@student.moringaschool.com>
2023-03-27 09:51:29 +0300

    show action for heroes

--- app/controllers/heroes_controller.rb
+++ app/controllers/heroes_controller.rb
@@ -3,4 +3,14 @@ class HeroesController < ApplicationController
        def index
         render json:Hero.all
     end
+
+      #GET /heroes/:id
+      def show
+        hero = Hero.find_by(id: params[:id])
+        if hero
+        render json: hero
+        else
+          render json: {errors: "Hero not found"}, status: :not_found
+        end
+      end
 end
